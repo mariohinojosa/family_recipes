104e966cd1384ad866a25e6cfc9a301706a01fce
Mario Hinojosa <mariohinojosa@gmail.com>
2017-10-10 10:50:21 -0500

    Added use of forms

---
+++ project/recipes/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import Form
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+
+class AddRecipeForm(Form):
+    """docstring for AddRecipeForm"""
+    recipe_title = StringField('Recipe Title', validators=[DataRequired()])
+    recipe_description = StringField('Recipe Description', validators=[DataRequired()])
