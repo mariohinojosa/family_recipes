03bbfa676a162129cf109cedc899a961e4f53982
Mario Hinojosa <mariohinojosa@gmail.com>
2017-10-09 19:55:37 -0500

    Added SQLAlchemy support and file for initializing database

---
+++ project/models.py
@@ -0,0 +1,16 @@
+from project import db
+
+class Recipe(db.Model):
+    """docstring for Recipe"""
+    __tablename__ = "recipes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_title = db.Column(db.String, nullable=False)
+    recipe_description = db.Column(db.String, nullable=False)
+
+    def __init__(self, title, description):
+        self.recipe_title = title
+        self.recipe_description = description
+
+    def __repr__(self):
+        return 'title {}'.format(self.name)
